c6854bda1ae3d1f7bd0c1534522e3a91ece1254c
pranavsuri96 <113058344+pranavsuri96@users.noreply.github.com>
2025-01-04 04:38:27 -0800

    Update app.js

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.get('/get-note/:noteId', async (req, res) => {
 });
 
 // Server port (Azure sets the port automatically, use process.env.PORT)
-const PORT = process.env.PORT || 10255;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
