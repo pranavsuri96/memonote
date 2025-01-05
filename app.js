84523ed56085bcbaa4ab03e6b6ae9a83a81c3886
Pranav Suri <pranavsuri96@gmail.com>
2025-01-04 18:56:27 -0800

    new appjs fole

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const mongoURI = 'mongodb://memonote:y3cWZyx3A7oNcycRLfQHAwo7czsi1twfvSYIrDi9l0K
 // Connect to MongoDB (Azure Cosmos DB)
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log('MongoDB connection error:', err));
 
 // Define a schema for a note
 const noteSchema = new mongoose.Schema({
@@ -84,3 +84,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
